7812cf51aa02ead0eef8246182007ef27c3b066b
Justin <73923621+truthinthorns@users.noreply.github.com>
2022-12-08 12:51:14 -0600

    Update routing.sh

--- routing.sh
+++ routing.sh
@@ -3,3 +3,4 @@ go get -u github.com/sirupsen/logrus
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
 sudo systemctl stop ufw
+sudo systemctl start mongod
